735250ae58600a338142116f4972f8af38a4b668
Alejandro <alejandrojuarez675@gmail.com>
2018-10-25 03:16:57 -0300

    Agrego lista dinamica de carouseles

---
+++ app/src/main/java/prueba/com/pruebatoolbox/AdaptadorRecycler.java
@@ -0,0 +1,73 @@
+package prueba.com.pruebatoolbox;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AdaptadorRecycler extends RecyclerView.Adapter<AdaptadorRecycler.ViewHolderRecycler> {
+
+    private ArrayList<Carousel> listCarouseles;
+
+    public AdaptadorRecycler(ArrayList<Carousel> listCarouseles) {
+        this.listCarouseles = listCarouseles;
+    }
+
+    public ArrayList<Carousel> getListCarouseles() {
+        return listCarouseles;
+    }
+
+    public void setListCarouseles(ArrayList<Carousel> listCarouseles) {
+        this.listCarouseles = listCarouseles;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderRecycler onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        @SuppressLint("InflateParams") View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_recycler, null, false);
+        return new ViewHolderRecycler(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderRecycler viewHolderRecycler, int i) {
+        viewHolderRecycler.tv_titulo.setText(listCarouseles.get(i).getTitle());
+        switch (listCarouseles.get(i).getType()){
+            case "thumb":
+                AdaptadorThumb adaptadorThumb = new AdaptadorThumb(listCarouseles.get(i).getItems());
+                viewHolderRecycler.rv_recycler.setAdapter(adaptadorThumb);
+                break;
+            case "poster":
+                AdaptadorPoster adaptadorPoster = new AdaptadorPoster(listCarouseles.get(i).getItems());
+                viewHolderRecycler.rv_recycler.setAdapter(adaptadorPoster);
+                break;
+            default:
+                    break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listCarouseles.size();
+    }
+
+    class ViewHolderRecycler extends RecyclerView.ViewHolder {
+
+        TextView tv_titulo;
+        RecyclerView rv_recycler;
+
+        ViewHolderRecycler(@NonNull View itemView) {
+            super(itemView);
+            tv_titulo   = itemView.findViewById(R.id.tv_recycler);
+            rv_recycler = itemView.findViewById(R.id.rv_recycler);
+            rv_recycler.setLayoutManager(new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.HORIZONTAL,false));
+
+        }
+    }
+}
--- app/src/main/java/prueba/com/pruebatoolbox/MainActivity.java
+++ app/src/main/java/prueba/com/pruebatoolbox/MainActivity.java
@@ -1,10 +1,9 @@
 package prueba.com.pruebatoolbox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,36 +16,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView rv_thumb = findViewById(R.id.rv_recycler_thumb);
-        RecyclerView rv_poster = findViewById(R.id.rv_recycler_poster);
-        TextView tv_titulo_thumb = findViewById(R.id.tv_titulo_thumb);
-        TextView tv_titulo_poster = findViewById(R.id.tv_titulo_poster);
-
-        rv_thumb.setLayoutManager(new LinearLayoutManager(
-                this, LinearLayoutManager.HORIZONTAL, false));
-        rv_poster.setLayoutManager(new LinearLayoutManager(
-                this, LinearLayoutManager.HORIZONTAL, false));
+        RecyclerView rv_recycler = findViewById(R.id.rv_lista_main);
+        rv_recycler.setLayoutManager(new LinearLayoutManager(
+                this, LinearLayoutManager.VERTICAL,false));
 
         cargarDatos();
 
-        AdaptadorThumb adaptadorThumb;
-        AdaptadorPoster adaptadorPoster;
-        for (int i=0; i<listaCaroucel.size();i++){
-            switch (listaCaroucel.get(i).getType()){
-                case "thumb":
-                    adaptadorThumb = new AdaptadorThumb(listaCaroucel.get(i).getItems());
-                    rv_thumb.setAdapter(adaptadorThumb);
-                    tv_titulo_thumb.setText(listaCaroucel.get(i).getTitle());
-                    break;
+        AdaptadorRecycler adaptadorRecycler = new AdaptadorRecycler(listaCaroucel);
+        rv_recycler.setAdapter(adaptadorRecycler);
 
-                case "poster":
-                    adaptadorPoster = new AdaptadorPoster(listaCaroucel.get(i).getItems());
-                    rv_poster.setAdapter(adaptadorPoster);
-                    tv_titulo_poster.setText(listaCaroucel.get(i).getTitle());
-                    break;
-            }
-        }
-
+//        RecyclerView rv_thumb = findViewById(R.id.rv_recycler_thumb);
+//        RecyclerView rv_poster = findViewById(R.id.rv_recycler_poster);
+//        TextView tv_titulo_thumb = findViewById(R.id.tv_titulo_thumb);
+//        TextView tv_titulo_poster = findViewById(R.id.tv_titulo_poster);
+//
+//        rv_thumb.setLayoutManager(new LinearLayoutManager(
+//                this, LinearLayoutManager.HORIZONTAL, false));
+//        rv_poster.setLayoutManager(new LinearLayoutManager(
+//                this, LinearLayoutManager.HORIZONTAL, false));
+//
+//        cargarDatos();
+//
+//        AdaptadorThumb adaptadorThumb;
+//        AdaptadorPoster adaptadorPoster;
+//        for (int i=0; i<listaCaroucel.size();i++){
+//            switch (listaCaroucel.get(i).getType()){
+//                case "thumb":
+//                    adaptadorThumb = new AdaptadorThumb(listaCaroucel.get(i).getItems());
+//                    rv_thumb.setAdapter(adaptadorThumb);
+//                    tv_titulo_thumb.setText(listaCaroucel.get(i).getTitle());
+//                    break;
+//
+//                case "poster":
+//                    adaptadorPoster = new AdaptadorPoster(listaCaroucel.get(i).getItems());
+//                    rv_poster.setAdapter(adaptadorPoster);
+//                    tv_titulo_poster.setText(listaCaroucel.get(i).getTitle());
+//                    break;
+//            }
+//        }
+//
     }
 
     private void cargarDatos(){
