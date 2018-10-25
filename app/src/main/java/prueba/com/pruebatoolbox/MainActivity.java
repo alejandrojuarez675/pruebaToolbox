03fc28983ee9a295e2be7a44d9278014448dfac1
Alejandro <alejandrojuarez675@gmail.com>
2018-10-25 02:31:49 -0300

    Preparo el switch para multiples carourseles

--- app/src/main/java/prueba/com/pruebatoolbox/MainActivity.java
+++ app/src/main/java/prueba/com/pruebatoolbox/MainActivity.java
@@ -10,11 +10,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView rv_thumb;
-    private RecyclerView rv_poster;
-    private TextView tv_titulo_thumb;
-    private TextView tv_titulo_poster;
-
     private ArrayList<Carousel> listaCaroucel;
 
     @Override
@@ -22,10 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rv_thumb        = findViewById(R.id.rv_recycler_thumb);
-        rv_poster       = findViewById(R.id.rv_recycler_poster);
-        tv_titulo_thumb = findViewById(R.id.tv_titulo_thumb);
-        tv_titulo_poster= findViewById(R.id.tv_titulo_poster);
+        RecyclerView rv_thumb = findViewById(R.id.rv_recycler_thumb);
+        RecyclerView rv_poster = findViewById(R.id.rv_recycler_poster);
+        TextView tv_titulo_thumb = findViewById(R.id.tv_titulo_thumb);
+        TextView tv_titulo_poster = findViewById(R.id.tv_titulo_poster);
 
         rv_thumb.setLayoutManager(new LinearLayoutManager(
                 this, LinearLayoutManager.HORIZONTAL, false));
@@ -34,43 +29,62 @@ public class MainActivity extends AppCompatActivity {
 
         cargarDatos();
 
-        AdaptadorThumb adaptadorThumb = new AdaptadorThumb(listaCaroucel.get(0).getItems());
-        AdaptadorPoster adaptadorPoster = new AdaptadorPoster(listaCaroucel.get(1).getItems());
-        rv_thumb.setAdapter(adaptadorThumb);
-        rv_poster.setAdapter(adaptadorPoster);
+        AdaptadorThumb adaptadorThumb;
+        AdaptadorPoster adaptadorPoster;
+        for (int i=0; i<listaCaroucel.size();i++){
+            switch (listaCaroucel.get(i).getType()){
+                case "thumb":
+                    adaptadorThumb = new AdaptadorThumb(listaCaroucel.get(i).getItems());
+                    rv_thumb.setAdapter(adaptadorThumb);
+                    tv_titulo_thumb.setText(listaCaroucel.get(i).getTitle());
+                    break;
 
-        tv_titulo_thumb.setText(listaCaroucel.get(0).getTitle());
-        tv_titulo_poster.setText(listaCaroucel.get(1).getTitle());
+                case "poster":
+                    adaptadorPoster = new AdaptadorPoster(listaCaroucel.get(i).getItems());
+                    rv_poster.setAdapter(adaptadorPoster);
+                    tv_titulo_poster.setText(listaCaroucel.get(i).getTitle());
+                    break;
+            }
+        }
+
     }
 
     private void cargarDatos(){
         listaCaroucel = new ArrayList<>();
         listaCaroucel.add(new Carousel("Carousel Thumb", "thumb"));
         listaCaroucel.get(0).addItem(
-                new Item("La playa","http://placeimg.com/320/480/any",
+                new Item("La playa",
+                        "http://placeimg.com/320/480/any",
                         "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
         listaCaroucel.get(0).addItem(
-                new Item("Peligro en Bangkok","http://placeimg.com/320/480/any",
+                new Item("Peligro en Bangkok",
+                        "http://placeimg.com/320/480/any",
                         "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
         listaCaroucel.get(0).addItem(
-                new Item("Todas contra John","http://placeimg.com/320/480/any",
+                new Item("Todas contra John",
+                        "http://placeimg.com/320/480/any",
                         "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
         listaCaroucel.get(0).addItem(
-                new Item("Quisiera ser millonario","http://placeimg.com/320/480/any",
+                new Item("Quisiera ser millonario",
+                        "http://placeimg.com/320/480/any",
                         "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
 
         listaCaroucel.add(new Carousel("Carousel Poster", "poster"));
         listaCaroucel.get(1).addItem(
-                new Item("La playa","http://placeimg.com/640/480/any",
+                new Item("La playa",
+                        "http://placeimg.com/640/480/any",
                 "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
         listaCaroucel.get(1).addItem(
-                new Item("Peligro en Bangkok","http://placeimg.com/640/480/any",
+                new Item("Peligro en Bangkok",
+                        "http://placeimg.com/640/480/any",
                         "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
         listaCaroucel.get(1).addItem(
-                new Item("Todas contra John","http://placeimg.com/640/480/any",
+                new Item("Todas contra John",
+                        "http://placeimg.com/640/480/any",
                         "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
         listaCaroucel.get(1).addItem(
-                new Item("Quisiera ser millonario","http://placeimg.com/640/480/any",
+                new Item("Quisiera ser millonario",
+                        "http://placeimg.com/640/480/any",
                         "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"));
     }
 }
